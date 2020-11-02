994569fc739cde9926bd047f7946844d13d0fa47
ThaiChubby <dangquocthai07061998@gmail.com>
2020-11-02 09:43:22 +0700

    add change-bg.js

---
+++ js/change-bg.js
@@ -0,0 +1,12 @@
+function firstBackgroundImage() {
+    document.getElementById("bg").style.backgroundImage =
+      "url(image/slide-1.png)";
+}
+function secondBackgroundImage() {
+    document.getElementById("bg").style.backgroundImage =
+      "url(image/slide-2.png)";
+}
+function thirdBackgroundImage() {
+    document.getElementById("bg").style.backgroundImage =
+      "url(image/slide-3.png)";
+}
