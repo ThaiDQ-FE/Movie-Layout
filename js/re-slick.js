c5e6d9f17fa1ad65fe2619bb8f65796683c65379
ThaiChubby <dangquocthai07061998@gmail.com>
2020-11-02 09:47:05 +0700

    re slick lib

--- js/re-slick.js
+++ js/re-slick.js
@@ -1,28 +1,55 @@
 $('.newIn__slide').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    infinite:true,
-    autoplay:false,
-    autoplaySpeed:3000,
-    nextArrow: '<i class="fa fa-angle-right"></i>',
-    prevArrow: '<i class="fa fa-angle-left"></i>',
-    responsive:[{
-        breakpoint:768,
-        settings:{slidesToShow:2}
-        },
-        {
-        breakpoint:480,
-        settings:{slidesToShow:1}
-        }]
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  infinite: true,
+  autoplay: false,
+  autoplaySpeed: 3000,
+  nextArrow: '<i class="fa fa-angle-right"></i>',
+  prevArrow: '<i class="fa fa-angle-left"></i>',
+  responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
 });
 
 
-window.onscroll = function() {scrollFunction()};
-            
-function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  speed: 10,
+  fade: true,
+  asNavFor: '.slider-nav',
+  draggable: false
+});
+$('.slider-nav').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  focusOnSelect: true,
+  nextArrow: '<i class="fa fa-angle-right"></i>',
+  prevArrow: '<i class="fa fa-angle-left"></i>',
+  responsive: [{
+    breakpoint: 1023,
+    settings: {
+      slidesToShow: 4
+    }
+  },
+  {
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 3
+    }
   }
-}
+]
+});
